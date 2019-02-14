9297b72ae66660b2ec0edf50f70f92bd6c429954
Emmanuel Valenzuela <32858351+Softx0@users.noreply.github.com>
2019-02-14 06:04:02 -0800

    Suite Example create for JUnitSuite

---
+++ src/test/java/SuiteExample.java
@@ -0,0 +1,12 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(value = Suite.class)
+@Suite.SuiteClasses({
+
+        CalculadoraCasioTest2.class,
+        CalculadoraCasioTest.class,
+        CalculadoraTest.class
+})
+public class SuiteExample {
+}
